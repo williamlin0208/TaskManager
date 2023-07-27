2c808236a9c5f83d6a0bbba5df938e1fbbabf5ed
unknown <yuchenweng7@realtek.com>
2023-07-27 17:28:39 +0800

    do part of AllTask

--- src/Utility/utility.js
+++ src/Utility/utility.js
@@ -1,12 +1,19 @@
 import { Modal, View, ActivityIndicator, Text } from "react-native";
 
 export const LoadingBar = ({ visible }) => (
-    <Modal onRequestClose={() => null} visible={visible}>
-      <View style={{ flex: 1, backgroundColor: '#dcdcdc', alignItems: 'center', justifyContent: 'center' }}>
-        <View style={{ borderRadius: 10, backgroundColor: 'white', padding: 25 }}>
-          <Text style={{ fontSize: 20, fontWeight: '200' }}>Loading</Text>
-          <ActivityIndicator size="large" />
-        </View>
+  <Modal onRequestClose={() => null} visible={visible}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#dcdcdc",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View style={{ borderRadius: 10, backgroundColor: "white", padding: 25 }}>
+        <Text style={{ fontSize: 20, fontWeight: "200" }}>Loading</Text>
+        <ActivityIndicator size="large" />
       </View>
-    </Modal>
-  );
+    </View>
+  </Modal>
+);
--- src/pages/Home/HomeScreen.js
+++ src/pages/Home/HomeScreen.js
@@ -1,44 +1,55 @@
-import React from 'react';
-import { useNavigation } from '@react-navigation/core';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import { useNavigation } from "@react-navigation/core";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import Bulletin from './Bulletin';
-import NewWork from './NewWork';
-import Notification, { NotificationDatail } from './Notification';
-import Settings from './Setting';
-import TaskDetail from '../../Utility/TaskDetail';
-import { TouchableOpacity } from 'react-native';
+import Bulletin from "./Bulletin";
+import NewWork from "./NewWork";
+import Notification, { NotificationDatail } from "./Notification";
+import Settings from "./Setting";
+import TaskDetail from "../../Utility/TaskDetail";
+import { TouchableOpacity } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 const HomeScreen = () => {
-
   const navigation = useNavigation();
 
   return (
     <Stack.Navigator initialRouteName="Bulletin">
-      <Stack.Screen name="Bulletin" 
-                    component={Bulletin} 
-                    options={() => ({
-                                      headerRight: () => 
-                                        <TouchableOpacity onPress={() => navigation.navigate("Settings")} style={{maxHeight:25,maxWidth:25,marginEnd:3}}>
-                                          <Ionicons name='settings-outline' size={25}/>
-                                        </TouchableOpacity>
-                                      ,
-                                      headerLeft: () => 
-                                        <TouchableOpacity onPress={() => navigation.navigate("Notification")} style={{maxHeight:25,maxWidth:25,marginStart:3}}>
-                                          <Ionicons name='notifications-outline' size={25}/>
-                                        </TouchableOpacity>
-                                      })}
+      <Stack.Screen
+        name="Bulletin"
+        component={Bulletin}
+        options={() => ({
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Settings")}
+              style={{ maxHeight: 25, maxWidth: 25, marginEnd: 3 }}
+            >
+              <Ionicons name="settings-outline" size={25} />
+            </TouchableOpacity>
+          ),
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Notification")}
+              style={{ maxHeight: 25, maxWidth: 25, marginStart: 3 }}
+            >
+              <Ionicons name="notifications-outline" size={25} />
+            </TouchableOpacity>
+          ),
+        })}
       />
       <Stack.Screen name="NewWork" component={NewWork} />
       <Stack.Screen name="Notification" component={Notification} />
       <Stack.Screen name="Settings" component={Settings} />
       <Stack.Screen name="HomeTaskDetail" component={TaskDetail} />
-      <Stack.Screen name='NotificationDetail' component={NotificationDatail} options={{title:'Detail'}} />
+      <Stack.Screen
+        name="NotificationDetail"
+        component={NotificationDatail}
+        options={{ title: "Detail" }}
+      />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
 export default HomeScreen;
